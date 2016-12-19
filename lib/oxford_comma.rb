c0247295befdab6447657700323f5b96705364db
Maria Ramirez <nanyramirez0@gmail.com>
2016-12-19 21:20:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  length=array.length
+  case length
+  when 1
+    array.join
+  when 2
+    array.join(" and ")
+  else
+    string=""
+    array.each do |item|
+      if array.index(item)== array.index(array.last)
+        string+="and #{item}"
+      else
+      string += "#{item}, "
+      end
+    end
+    string 
+  end
 end
